2e7f83c742fb26365a36325e9ae310978e467d3e
B96152 <PaulHH03191975@fullsail.edu>
2013-01-18 12:40:51 -0600

    Added Variables and Output Activity for SDI 1301.

---
+++ Variablesandoutputs/var-and-output.js
@@ -0,0 +1,19 @@
+// alert("JavaScript works!");
+
+// Paul Holmes
+// SDI 1301
+// Variables and Output activity
+
+// My variables
+
+var myDogName = "Butch";
+var hasFleas = true;
+var numOfFleas = 50;
+var dogShampoo = "\"Magic Flea\" shampoo";
+
+// My ouput
+console.log("My dog's name is " + myDogName);
+console.log("It is " + hasFleas + " that he has fleas.");
+console.log("I've counted at least " + numOfFleas + " of them");
+console.log("So, he's getting a bath with " + dogShampoo + " tonight!");
+
